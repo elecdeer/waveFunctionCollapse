944dde3699b82faf1527acae0f53a1391af69991
takumi <t.kashima05@gmail.com>
2020-04-16 00:02:58 +0900

    Initial commit

---
+++ src/Core/Main.java
@@ -0,0 +1,29 @@
+package Core;
+
+import processing.core.*;
+
+public class Main extends PApplet{
+	public static PApplet P5;
+
+	@Override
+	public void settings(){
+		P5 = this;
+
+		size(400, 300, P2D);
+	}
+
+	@Override
+	public void setup(){
+
+	}
+
+	@Override
+	public void draw(){
+
+	}
+
+	public static void main(String[] args){
+		PApplet.main("Core.Main");
+	}
+}
+
